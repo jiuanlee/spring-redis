63d3b1fe2a93251223c68b298c3f8c2d5a804047
lijiuan <lijiuan@hd123.com>
2019-08-19 15:34:23 +0800

    redisUtil工具类优化

--- src/main/java/com/ssm/utils/RedisUtil.java
+++ src/main/java/com/ssm/utils/RedisUtil.java
@@ -2,18 +2,12 @@ package com.ssm.utils;
 
 import com.ssm.biz.ApplicationContextUtils;
 import com.ssm.biz.LatinConstants;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 
-import javax.annotation.Resource;
 import java.util.concurrent.TimeUnit;
 
 public class RedisUtil {
     private static RedisUtil instance = new RedisUtil();
-    @Resource(name = "redisTemplate")
-    private RedisTemplate<String, String> redisTemplate;
 
     public static RedisUtil getInstance() {
         return instance;
